16678d30e2331299cd7d15e66a83bb9e4152ed22
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2023-12-06 15:24:33 -0300

    Controller de perfil

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,7 +1,19 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use.case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify()
-  console.log(request.user.sub)
+  const getUserprofile = makeGetUserProfileUseCase()
+
+  const { user } = await getUserprofile.execute({
+    userId: request.user.sub,
+  })
+
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  })
+
   return reply.status(200).send()
 }
