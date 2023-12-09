e366041b00152c4c934427d47dfe525a6e9380db
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2023-12-09 20:31:37 -0300

    Teste E2E do perfil

--- src/http/controllers/profile.spec.ts
+++ src/http/controllers/profile.spec.ts
@@ -1,3 +1,39 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {})
+describe('Profile (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to get user profile', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const authResponse = await request(app.server).post('/sessions').send({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const { token } = authResponse.body
+
+    const profileReponse = await request(app.server)
+      .get('/me')
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(profileReponse.statusCode).toBe(200)
+    expect(profileReponse.body.user).toEqual(
+      expect.objectContaining({
+        email: 'johndoe@example.com',
+      }),
+    )
+  })
+})
