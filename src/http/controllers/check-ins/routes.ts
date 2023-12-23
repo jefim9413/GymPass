772168a597275f9866097796ef6350f69c466d85
Jefferson Marques <jefferson.jsm3444@gmail.com>
2023-12-22 22:59:16 -0300

    Controllers das rotas de check-ins

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -1,1 +1,10 @@
 import { FastifyInstance } from 'fastify'
+
+import { verifyJwt } from '@/http/middlewares/verify-jwt'
+import { create } from './create'
+
+export async function checkInsRoutes(app: FastifyInstance) {
+  app.addHook('onRequest', verifyJwt)
+
+  app.post('/gyms/:gymId/check-ins', create)
+}
