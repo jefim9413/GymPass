65fcb287276a24c40e8329bb18435edd797f8336
Jefferson Marques <jefferson.jsm3444@gmail.com>
2023-12-22 22:12:35 -0300

    Outros controllers da academia

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,29 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { makeSearchGymsUseCase } from '@/use-cases/factories/make-search-gyms-use-case'
+import { makeFetchNearbyGymsUseCase } from '@/use-cases/factories/make-fetch-nearby-gyms-use-case'
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const nearbyGymsQuerySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { latitude, longitude } = nearbyGymsQuerySchema.parse(request.query)
+
+  const fetchNeabyGymsUseCase = makeFetchNearbyGymsUseCase()
+
+const { gyms } = await fetchNeabyGymsUseCase.execute({
+  userLatitude: latitude,
+  userLongitude: longitude,
+})
+
+
+  return reply.status(201).send({
+    gyms
+  })
+}
---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,32 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { makeSearchGymsUseCase } from '@/use-cases/factories/make-search-gyms-use-case'
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymsQuerySchema = z.object({
+    q: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  })
+
+  const { q, page } = searchGymsQuerySchema.parse(request.query)
+
+  const searchGymsUseCase = makeSearchGymsUseCase()
+
+const { gyms } = await searchGymsUseCase.execute({
+  query: q,
+  page,
+})
+
+  const searchGymUseCase = makeSearchGymsUseCase()
+
+  await searchGymUseCase.execute({
+    query: q,
+    page,
+  })
+
+
+
+  return reply.status(201).send({
+    gyms
+  })
+}
